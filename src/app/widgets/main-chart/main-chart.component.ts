99b8bf870bcdddd289af8fec700860725b55bee9
siddhanth339 <sidrk339@gmail.com>
2025-03-29 18:07:57 -0500

    added panes from lightweight charts to display volumes of stocks

--- src/app/widgets/main-chart/main-chart.component.ts
+++ src/app/widgets/main-chart/main-chart.component.ts
@@ -48,18 +48,22 @@ export class MainChartComponent implements OnInit, OnDestroy {
 
   private createPriceChart(): void {
     this.priceChartOptions = {
-      layout: { textColor: 'black', background: { type: 'solid', color: 'white' } },
+      layout: { textColor: 'black', background: { type: 'solid', color: 'white' }, panes: {
+        separatorColor: '#f22c3d',
+        separatorHoverColor: 'rgba(255, 0, 0, 0.1)',
+        // setting this to false will disable the resize of the panes by the user
+        enableResize: false,
+    } },
       autoSize: true
     };
     this.priceChart = createChart('priceChartContainer', this.priceChartOptions);
 
-    // top parameter sets 0.7 or 70% empty space from the top of the chart
-    // bottom sets 0% from the bottom of the chart to make the chart align with the x axis
-    this.priceChart.priceScale('right').applyOptions({
-
-    });
+    // 0 at the end set's the pane index for charts. Panes are like separations within a chart to display multiple series
    const candlestickSeries = this.priceChart.addSeries(CandlestickSeries, { 
-      upColor: '#26a69a', downColor: '#ef5350', borderVisible: false, wickUpColor: '#26a69a', wickDownColor: '#ef5350' });
+      upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
+      wickUpColor: '#26a69a', wickDownColor: '#ef5350',
+      title: 'Price' }, 
+      0);
 
     let chartData = [];
 
@@ -74,13 +78,38 @@ export class MainChartComponent implements OnInit, OnDestroy {
     }
     chartData.reverse(); // to have data sorted from oldest to latest price on the price chart from left to right
     candlestickSeries.setData(chartData);
+
+    const volumeSeries = this.priceChart.addSeries(HistogramSeries, { color: '#26a69a', title: 'Volume'}, 1);
+    chartData = [];
+    let prevValue = -1;
+    let currentBarColor = '#26a69a';
+
+    for (let key in this.priceData["Monthly Time Series"]) {
+      const val = parseFloat(this.priceData['Monthly Time Series'][key]['5. volume']) / 1000000000;
+      chartData.push({
+        value: val,
+        time: key,
+        color: ''
+      })
+      prevValue = val;
+    }
+    chartData.reverse();
+
+    chartData.map((item, index, array) => {
+      if (index > 0) {
+        const previousItem = array[index - 1];
+        item.color = item.value > previousItem.value ? '#26a69a' : '#ef5350';
+      } else {
+          item.color = '#26a69a'; //set the first item to gray or any other color you want.
+      }
+      return item;
+    })
+    
+    volumeSeries.setData(chartData);
+
     //Adjust the chart view to fit content
     this.priceChart.timeScale().fitContent();
   }
-
-
-
-
 
   ngOnDestroy() {
     if (this.subscription) {
@@ -88,6 +117,3 @@ export class MainChartComponent implements OnInit, OnDestroy {
     }
   }
 }
-
-
-// volumes are generally in hundreds of thousands but price can range from < 100 to a few thousand
